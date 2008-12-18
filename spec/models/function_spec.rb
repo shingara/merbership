f9ecddb72962f76fb735d7919e7f394acbba8cf7
Cyril Mougel <cyril.mougel@gmail.com>
2008-12-18 23:52:20 +0100

    add spec to function model

--- spec/models/function_spec.rb
+++ spec/models/function_spec.rb
@@ -2,6 +2,10 @@ require File.join( File.dirname(__FILE__), '..', "spec_helper" )
 
 describe Function do
 
-  it "should have specs"
+  it "should get all non admin" do
+    Function.gen(:president)
+    Function.gen(:member)
+    Function.not_admin.should have(1).item
+  end
 
 end
