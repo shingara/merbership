9a4a995b62b9402ab47cabc84bdbc1448bb78e2a
Cyril.mougel <cyril.mougel@gmail.com>
2008-12-21 18:56:48 +0100

    delete all member of function if function destroy

--- app/models/function.rb
+++ app/models/function.rb
@@ -7,6 +7,12 @@ class Function
 
   has n, :members
 
+  # There are no "cascading" delete in DM-0.9.8
+  # it's plan in dm-constraint for futur
+  after :destroy do
+    Member.all(:function_id.not => Function.all.inject([]) {|h,f| h << f.id}).destroy
+  end
+
   def self.not_admin
     all(:admin => false)
   end
